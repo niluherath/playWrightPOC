88d04290de4ad7ffb28bdc246f583016a3d0ba7b
nilu herath <niluherath@hotmail.com>
2024-10-24 00:12:01 +1300

    add .env file

--- tests/homePageValidation.spec.ts
+++ tests/homePageValidation.spec.ts
@@ -8,7 +8,9 @@ test('validate Home page content', async ({ page }) => {
   const global_url = process.env.GLOBAL_URL as string;  
   console.log('GLOBAL_URL:', global_url); 
 
- await page.goto(global_url);
+  const user_name = process.env.USER_NAME as string;  
+  console.log('user_name:', user_name); 
+  await page.goto(global_url);
 
   await expect(page).toHaveTitle('playwright-test-admin Demo Application')
   const light = page.getByText('Roller Shades')
